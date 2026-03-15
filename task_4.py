394338bf1fe7dae8a977ed0036dd89f1a7610fb0
Pushihvost <diabl1k254@gmail.com>
2026-03-15 11:09:00 +0300

    Update task_4.py: delete comments

--- task_4.py
+++ task_4.py
@@ -8,12 +8,6 @@
         self.rest_days = rest_days
         self.email = email
 
-        #print("Создан объект EmployeeSalary:")
-        #print("Имя:", self.name)
-        #print("Часы:", self.hours)
-        #print("Выходные:", self.rest_days)
-        #print("Email:", self.email)
-
     @classmethod
     def get_hours(cls, name, rest_days, email):
         hours = (7 - rest_days) * 8
@@ -30,7 +24,3 @@
     
     def salary(self):
         return self.hours * self.hourly_payment
-
-#emp = EmployeeSalary("Alex", 40, 2, "alex@email.com")
-#emp2 = EmployeeSalary.get_hours("Nastya", 5, "nastya@email.com")
-#EmployeeSalary.get_email("Dima", 30, 3)
